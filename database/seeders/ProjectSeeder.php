64f15d44952c0e4e85443c51809f48a2123398f2
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-02-14 17:28:07 +0100

    added faker and type_id col in the project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,20 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $projects = config('projects.project');
+
+        $typeIds = Type::all()->pluck('id');
+
         foreach ($projects as $project) {
             $newProject = new Project();
             $newProject->title = $project['title'];
+            $newProject->type_id = $faker->randomElement($typeIds);
             $newProject->description = $project['description'];
             $newProject->project_url = $project['project_url'];
             $newProject->used_languages = $project['used_languages'];
