5a45b2898a508a564939dcec92dc44be0e190d1b
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-02-14 17:13:59 +0100

    seeded types table

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'Front-end',
+            'Back-end',
+            'Full-stack',
+            'Design',
+        ];
+
+        foreach ($types as $singleType) {
+            $type = new Type();
+            $type->title = $singleType;
+            $type->save();
+        }
     }
 }
